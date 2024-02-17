beca9d0615514da644f542950abdcf6e1ff28802
Amit Kumar <amitmalhotra2050@gmail.com>
2024-02-17 17:23:09 +0530

    Hello World Controller added

--- src/main/java/com/test/amit/demo/controller/HelloWorldController.java
+++ src/main/java/com/test/amit/demo/controller/HelloWorldController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class HelloWorldController {
 
-    @GetMapping("")
+    @GetMapping("/world")
     public String abc(){
         return "as";
     }
